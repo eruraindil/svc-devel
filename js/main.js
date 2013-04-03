8050edf28f23c040b60a4f16e9ff087f0d08f959
Matthew Roberts <mrober13@uoguelph.ca>
2013-04-02 20:51:22 -0400

    updates to style and proper header usage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+if (matchMedia('only (max-width: 767px)').matches) {
+
+} else {
+    $('#highlights').affix({
+        offset: {
+            top: 100,
+            bottom: 140
+        }
+    })
+}
