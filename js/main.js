fde3828e3cddce77e4f4417b00047912a38493d6
Matthew Roberts <mrober13@uoguelph.ca>
2013-04-06 13:56:19 -0400

    updating affix and contact button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 
-if (matchMedia('only (max-width: 767px)').matches) {
-
+if(Modernizr.mq('only screen and (max-width: 767px)')) {
+    $('#highlights').css('background-image','none');
 } else {
     $('#highlights').affix({
         offset: {
             top: 100,
             bottom: 140
         }
-    })
+    });
 }
+/*
+if(Modernizr.mq('only all')) {
+    document.write("test!");
+}*/
